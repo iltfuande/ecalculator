0aeb79d9a5b12ab754e1aee2e47bf21979e25a0b
iltfuande <iltfuande@gmail.com>
2023-12-11 23:36:29 +0200

    Create interfaces of calculator

---
+++ src/main/java/ua/terokhin/demo/interfaces/CalculatorService.java
@@ -0,0 +1,11 @@
+package ua.terokhin.demo.interfaces;
+
+public interface CalculatorService {
+    double add(double a, double b);
+
+    double subtract(double a, double b);
+
+    double multiply(double a, double b);
+
+    double divide(double a, double b);
+}
---
+++ src/main/java/ua/terokhin/demo/interfaces/LogarithmCalculatorService.java
@@ -0,0 +1,7 @@
+package ua.terokhin.demo.interfaces;
+
+public interface LogarithmCalculatorService {
+    double naturalLogarithm(double number);
+
+    double base10Logarithm(double number);
+}
